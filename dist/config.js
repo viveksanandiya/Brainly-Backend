6fe88def1af6d9eb9ed65c8fd615e678bd1de1c6
Vivek-Sanandiya <viveksanandiya787@gmail.com>
2025-02-20 12:39:39 +0530

    gitignore

--- dist/config.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_PASSWORD = void 0;
-exports.JWT_PASSWORD = "123123";
--- src/config.ts
+++
@@ -1,1 +0,0 @@
-export const JWT_PASSWORD = "123123";

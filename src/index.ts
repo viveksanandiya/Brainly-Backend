e0960c5edd0564c8c19ad2a44ae6b8a8f1614f78
Vivek-Sanandiya <viveksanandiya787@gmail.com>
2025-05-10 19:32:02 +0530

    addded cors origin frontend link

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ''
+  origin: 'https://brainly-frontend-u9oi.onrender.com'
 }));
 
 app.post("/api/v1/signup", async (req, res) => {
